3932c0a1772dad59d6d97d50fb092d8b0d36e361
Chas Linn <chaslinn@users.noreply.github.com>
2025-05-08 16:27:35 +0100

    steel update

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,11 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.get('/question1', function (req, res) {
+  if (req.session.data.proceed === 'can proceed') {
+    res.redirect('/tasklist1');
+    }
+  else 
+    res.redirect('/ftf/steel/keeper-no');
+});
